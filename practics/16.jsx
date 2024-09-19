27d03669e5309747cb917b76d75e0b1c3236e9e0
dmitry-podkyuko <d.podkuyko@nv.dunice.net>
2024-09-19 14:36:22 +0300

    add 16 task

---
+++ practics/16.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// 1. Необходимо вывести список items на странице
+// 2. Сделайте input и button для добавления нового item
+// 3. Добавьте возможность менять completed у любого items, пусть будет отображаться как checkbox
+
+const initialItems = [
+  { id: 1, label: 'Learn Typescript', completed: true },
+  { id: 2, label: 'Learn React', completed: false },
+  { id: 3, label: 'Learn Vue', completed: false },
+  { id: 4, label: 'Learn Angular', completed: false },
+];
+
+const filterOptions = ['all', 'completed', 'uncompleted'];
+
+export default function App() {
+  return <></>;
+}
